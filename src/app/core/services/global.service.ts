d0cffcd04528c0896e5cbf41ff406899cd42623d
Alin Luca <alinluca20021@gmail.com>
2024-01-26 04:51:13 +0200

    Changed server url

--- src/app/core/services/global.service.ts
+++ src/app/core/services/global.service.ts
@@ -6,8 +6,8 @@ import {UserService} from "../http/user-service/user.service";
   providedIn: 'root'
 })
 export class GlobalService {
-  baseURL: string = "http://localhost:8080/api/v1";
-  // baseURL: string = "https://social-media-app-java.azurewebsites.net/api/v1";
+  // baseURL: string = "http://localhost:8080/api/v1";
+  baseURL: string = "https://social-media-app-java.azurewebsites.net/api/v1";
   user: UserDTO | null = null;
   constructor() { }
 
